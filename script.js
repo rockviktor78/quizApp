1e0bf7fb6cca517f6201e23e33c7bf15f793ffa4
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2025-09-18 12:10:57 +0200

    feat: update question and answer display in index.html and script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ let questions = [
     },
 ];
 
-
+let currentQuestion = 0;
 
 
 function init() {
@@ -68,9 +68,9 @@ function init() {
 
 function showQuestion() {
     let question = questions[currentQuestion];
-    document.getElementById('question-text').innerHTML = question['question'];
-    // document.getElementById('answer_1').innerHTML = question['answer_1'];
-    // document.getElementById('answer_2').innerHTML = question['answer_2'];
-    // document.getElementById('answer_3').innerHTML = question['answer_3'];
-    // document.getElementById('answer_4').innerHTML = question['answer_4'];
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
