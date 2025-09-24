271e92015a87d2514015c670f575b3237dbfb0e6
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2025-09-24 05:23:42 +0200

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -61,6 +61,8 @@ let questions = [
 let rightQuestions = 0;
 
 let currentQuestion = 0;
+let AUDIO_SUCCESS = new Audio("sounds/success.mp3");
+let AUDIO_FAIL = new Audio("sounds/fail.mp3");
 
 function init() {
   document.getElementById("all-questions").innerHTML = questions.length;
@@ -68,56 +70,56 @@ function init() {
   showQuestion();
 }
 
-
-
 function showQuestion() {
-  
-  
   if (currentQuestion >= questions.length) {
     // Show end screen
-    document.getElementById("endScreen").style = '';
-    document.getElementById("questionBody").style = 'display: none';
+    document.getElementById("endScreen").style = "";
+    document.getElementById("questionBody").style = "display: none";
 
     document.getElementById("amount-of-questions").innerHTML = questions.length;
-    document.getElementById("amount-of-right-questions").innerHTML = rightQuestions;
+    document.getElementById("amount-of-right-questions").innerHTML =
+      rightQuestions;
     document.getElementById("header-image").src = "Quizapp/brain_result.png";
-   } else { // Show question
+  } else {
+    // Show question
 
     let percent = (currentQuestion + 1) / questions.length;
     percent = Math.round(percent * 100);
-  
-    console.log('Fortschritt',percent);
+
+   
     document.getElementById("progress-bar").innerHTML = `${percent} %`;
     document.getElementById("progress-bar").style = `width: ${percent}%`;
 
-  let question = questions[currentQuestion];
-  
-  document.getElementById("question-number").innerHTML = currentQuestion + 1;
-  document.getElementById("questionText").innerHTML = question["question"];
-  document.getElementById("answer_1").innerHTML = question["answer_1"];
-  document.getElementById("answer_2").innerHTML = question["answer_2"];
-  document.getElementById("answer_3").innerHTML = question["answer_3"];
-  document.getElementById("answer_4").innerHTML = question["answer_4"];
+    let question = questions[currentQuestion];
+
+    document.getElementById("question-number").innerHTML = currentQuestion + 1;
+    document.getElementById("questionText").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
   }
 }
-
-
-
-
 
 function answer(selection) {
   let question = questions[currentQuestion];
   let selectedQestionNumber = selection.slice(-1);
   let idOfRightAnswer = `answer_${question["right_answer"]}`;
 
-  if (selectedQestionNumber == question["right_answer"]) { // Richtige Frage beantwortet
+  if (selectedQestionNumber == question["right_answer"]) {
+    // Richtige Frage beantwortet
     document.getElementById(selection).parentNode.classList.add("bg-success");
+    AUDIO_SUCCESS.play();
     rightQuestions++;
   } else {
     document.getElementById(selection).parentNode.classList.add("bg-danger");
-    document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
+    document
+      .getElementById(idOfRightAnswer)
+      .parentNode.classList.add("bg-success");
+      AUDIO_FAIL.play();
   }
-   document.getElementById("next-button").disabled = false;
+  
+  document.getElementById("next-button").disabled = false;
 }
 
 function nextQuestion() {
@@ -136,4 +138,15 @@ function resetAnswersButtons() {
   document.getElementById("answer_3").parentNode.classList.remove("bg-success");
   document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
   document.getElementById("answer_4").parentNode.classList.remove("bg-success");
+}
+
+function restartGame() {
+  document.getElementById("header-image").src = "img/pencil.jpg";
+  document.getElementById("questionBody").style = ""; // Show question body
+  document.getElementById("endScreen").style = "display: none"; // Hide end screen
+  rightQuestions = 0;
+  currentQuestion = 0;
+  resetAnswersButtons();
+  showQuestion();
+  init();
 }
